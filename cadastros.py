5c1b8a5ff5518e1afafa81e6031a9e5dcc699a90
Izabella Araujo <izabellaa244@gmail.com>
2025-01-05 14:29:59 -0400

    outras alteracoes

--- cadastros.py
+++ cadastros.py
@@ -1,6 +1,7 @@
 alunos = []
 professores = []
 disciplinas = []
+turmas = []
 
 def cadastrar_aluno(nome, matricula, data_de_nascimento, sexo, endereco, telefone, email):
     aluno = {
@@ -13,6 +14,13 @@ def cadastrar_aluno(nome, matricula, data_de_nascimento, sexo, endereco, telefon
         "email": email
     }
     alunos.append(aluno)
+    nome = input("Nome do aluno:")
+    matricula = input("Matricula:")
+    data_de_nascimento = int(input("Data de nascimento:(dd/mm/aaaa)"))
+    sexo = input("Sexo: m/f")
+    endereco = input("Endereco:")
+    telefone = int(input("telefone:"))
+    email = input("e-mail:")
 
 def cadastrar_professor(nome, matricula, data_de_nascimento, sexo, endereco, telefone, email, disciplina):
     professor = {
@@ -25,5 +33,40 @@ def cadastrar_professor(nome, matricula, data_de_nascimento, sexo, endereco, tel
         "email": email,
         "disciplina": disciplina
     }
+    professores.append(professor)
+    nome = input("Nome do professor:")
+    matricula = input("Matricula:")
+    data_de_nascimento = int(input("Data de nascimento:(dd/mm/aaaa)"))
+    sexo = input("Sexo: m/f")
+    endereco = input("Endereco:")
+    telefone = int(input("Telefone:"))
+    email = input("E-mail:")
+    disciplina = input("Disciplina")
 
-def cadastrar_disciplina():
+def cadastrar_disciplina(nome, codigo, carga_horaria, professor):
+    disciplina = {
+        "nome": nome,
+        "codigo": codigo, 
+        "carga_horaria": carga_horaria,
+        "professor": professor
+    }
+    disciplinas.append(disciplina)
+    nome = input("Nome da disciplina:")
+    codigo = input("Digite o codigo:")
+    carga_horaria = int(input("Digite a carga horaria:"))
+    professor = input("Professor:")
+
+def cadastrar_turma(nome, codigo, disciplina, professor, alunos):
+    turma = {
+        "nome": nome,
+        "codigo": codigo,
+        "disciplina": disciplina,
+        "professor": professor,
+        "alunos": alunos
+    }
+    turmas.append(turma)
+    nome = input("Nome da disciplina:")
+    codigo = input("Digite o codigo:")
+    disciplina = input("Digite a disciplina:")
+    professor = input("Professor:")
+    alunos = input("Alunos:")
