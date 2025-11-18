b6068aa700b99e1d8c96fd797f5cc81622a84be8
Izabella Araujo <izabellaa244@gmail.com>
2025-11-17 23:00:47 -0300

    cria file para excecoes

---
+++ Banco/excecoes.py
@@ -0,0 +1,47 @@
+class SaldoInsuficienteError(Exception):
+    #Erro quando o saque é maior que o saldo disponível."""
+    pass
+
+
+class LimiteExcedidoError(Exception):
+    #Erro quando o saque ultrapassa o limite permitido."""
+    pass
+
+
+class ValorInvalidoError(Exception):
+    #Erro quando o valor informado é negativo, zero ou não numérico."""
+    pass
+
+class SenhaInvalidaError(Exception):
+    #Erro quando a senha informada é inválida."""
+    pass
+
+class ContaInexistenteError(Exception):
+    #Erro quando a conta informada não existe."""
+    pass
+
+class OperacaoNaoPermitidaError(Exception):
+    #Erro quando a operação não é permitida."""
+    pass
+
+class PrazoNaoVencidoError(Exception):
+    #Erro quando o prazo do empréstimo não foi vencido."""
+    pass        
+
+class SaldoInsuficienteParaManutencaoError(Exception):
+    #Erro quando o saldo é insuficiente para cobrança de taxa de manutenção."""
+    pass    
+
+class AniversarioNaoCoincideError(Exception):
+    #Erro quando o dia do aniversário não coincide com a data atual."""
+    pass
+
+class DataPagamentoInvalidaError(Exception):
+    #Erro quando a data de pagamento é inválida."""
+    pass    
+
+class ParcelaInvalidaError(Exception):
+    #Erro quando o valor da parcela é inválido."""
+    pass     
+
+# =========================
