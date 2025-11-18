2d3886ce13808669d7f7604a491230612e149a3b
Izabella Araujo <izabellaa244@gmail.com>
2025-11-17 22:37:05 -0300

    adiciona menu mais organizado

--- Banco/menu.py
+++ Banco/menu.py
@@ -1,6 +1,8 @@
-from banco import sacar, depositar, extrato
+from banco import Conta_Corrente
 
 def menu():
+   # Criando uma conta de exemplo para os testes
+    conta = Conta_Corrente("001", "Fulano", "1234", 500, 200)
 
     while True:
         print('\n############# BEM VINDO AO MENU DE OPERACOES ############\n')
@@ -9,24 +11,32 @@ def menu():
         print('3 - Extrato')
         print('0 - Sair')
 
-        op = input('Digite a operacaoo que deseja prosseguir:')
+        op = input('Digite a operacao que deseja prosseguir: ')
+        #essa verificação é para evitar que o programa quebre caso o usuário digite algo que não seja número
+        try:
+            op = int(op)
+        except: 
+            print("Opção inválida")
+            continue
 
-        if op  == 1:
-            valor = float(input('Digite o valor do saque:'))
-            saldo = sacar(saldo, valor)
-        
+        if op == 1:
+            valor = float(input('Digite o valor do saque: '))
+            conta.sacar(valor)
+
         elif op == 2:
-            valor = float(input('Digite o valor a ser depositado:'))
-            saldo = depositar(saldo, valor)
-        
+            valor = float(input('Digite o valor a ser depositado: '))
+            conta.depositar(valor)
+
         elif op == 3:
-            pass
-            
-        elif op == 4:
+            conta.print_extrato()
+
+        elif op == 0:
             print('Saindo...')
             break
+
         else:
             print('Ops, digite uma opcao valida')
-                 
+
+
 if __name__ == "__main__":
     menu()
