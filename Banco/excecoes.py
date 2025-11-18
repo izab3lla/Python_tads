2dd7082eb696f05aae43001e65af1eadddc5dc4f
Izabella Araujo <izabellaa244@gmail.com>
2025-11-17 23:25:37 -0300

    adiciona excecoes

--- Banco/excecoes.py
+++ Banco/excecoes.py
@@ -1,33 +1,31 @@
 class SaldoInsuficienteError(Exception):
-    #Erro quando o saque é maior que o saldo disponível."""
+    """Erro quando o saque é maior que o saldo disponível."""
     pass
 
 class LimiteExcedidoError(Exception):
-    #Erro quando o saque ultrapassa o limite permitido."""
+    """Erro quando o saque ultrapassa o limite permitido."""
     pass
 
 class ValorInvalidoError(Exception):
-    #Erro quando o valor informado é negativo, zero ou não numérico."""
+    """Erro quando o valor informado é negativo, zero ou não numérico."""
     pass
 
 class SenhaInvalidaError(Exception):
-    #Erro quando a senha informada é inválida."""
+    """Erro quando a senha informada é inválida."""
     pass
 
 class ContaInexistenteError(Exception):
-    #Erro quando a conta informada não existe."""
+    """Erro quando a conta informada não existe."""
     pass
 
 class OperacaoNaoPermitidaError(Exception):
-    #Erro quando a operação não é permitida."""
+    """Erro quando a operação não é permitida."""
     pass
 
 class PrazoNaoVencidoError(Exception):
-    #Erro quando o prazo do empréstimo não foi vencido."""
+    """Erro quando o prazo do empréstimo não foi vencido."""
     pass        
 
 class SaldoInsuficienteParaManutencaoError(Exception):
-    #Erro quando o saldo é insuficiente para cobrança de taxa de manutenção."""
-    pass    
-
-# =========================
+    """Erro quando o saldo é insuficiente para cobrança de taxa de manutenção."""
+    pass
